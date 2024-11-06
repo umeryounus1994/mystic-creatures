e6a394f5073c0806cbca352e1811f397d06a18aa
umeryounus1994 <umeryounus92@gmail.com>
2024-11-06 22:16:25 +0500

    msgss

--- src/v1/controllers/treasurehunt.controller.js
+++ src/v1/controllers/treasurehunt.controller.js
@@ -114,7 +114,6 @@ const updateTreasureHuntAdmin = async (req, res, next) => {
       treasure_hunt_end_date: req.body?.treasure_hunt_end_date,
       hunt_location: location,
       premium_hunt: req.body?.premium_hunt,
-      qr_code: req.body?.qr_code,
       hunt_package: req.body?.hunt_package != "null" ? req.body?.hunt_package : undefined,
       reward_file: req.files['reward'] ? req.files['reward'][0].location : req.body.reward_file
     };
